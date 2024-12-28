a3500bfac32a3c8e93967748d37ff53f368e7709
alokbiradar <146175149+alokbiradar@users.noreply.github.com>
2024-12-28 15:25:03 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const react = require("react");
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome To Devtown</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,17 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div class="note">
+      <h1>Frontend Bootcamp Using Reactjs</h1>
+      <p>
+        I learnt what is Frontend from scratch,Frontend is
+        nothing but the part of the website which interacts with the user,we
+        also learnt how to use Reactjs to create front end and completed a
+        project based on it at the end of bootcamp
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
